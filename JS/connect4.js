eb3fe0c1946c447d3bdcaf15d81516805154c992
Brantley Burton <brantleyburton@Brantleys-MacBook-Air.local>
2018-06-18 14:42:15 -0400

    added cells to the circles

--- JS/connect4.js
+++ JS/connect4.js
@@ -1,3 +1,15 @@
+let alternate = true;
+const $changeBackground = (event) => {
+  const $clickedCircle = $(event.target)
+  console.log('executing background changeBackground')
+  if (alternate === true) {
+    $clickedCircle.addClass('orange');
+    alternate = false;
+  } else {
+    $clickedCircle.addClass('green');
+    alternate = true
+  }
+}
 //make a board
 const $circle = $('.circle');
 const makeBoard = () => {
@@ -11,9 +23,19 @@ const makeBoard = () => {
     $row.append($column);
     $column.addClass('empty-column');
     $column.attr('columnNumber', column);
+    const $cell = $('<div>');
+      $cell.addClass('column')
+      $cell.addClass('none')
+      $cell.attr('rowNumber', row)
+      $cell.attr('columnNumber', column)
+      $cell.on('click', $changeBackground);
+      $row.append($cell);
     }
   }
 }
+
+
+
 
 $(() => {
 makeBoard();
