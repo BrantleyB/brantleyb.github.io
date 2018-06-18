df2accd2200361207778d08bd84911c74618587d
Brantley Burton <brantleyburton@Brantleys-MacBook-Air.local>
2018-06-18 13:27:08 -0400

    created the board using a loop

--- JS/connect4.js
+++ JS/connect4.js
@@ -1,13 +1,20 @@
+//make a board
 const $circle = $('.circle');
-
-
-
-$circle.on('click' ()=> {
-  $(event.currentTarget).css('background-color', 'red');
-
-})
-
+const makeBoard = () => {
+  for (let row = 1; row <= 7; row++) {
+  let $row = $('<div>');
+  $('.board').append($row);
+  $row.addClass('row');
+  $row.attr('rowNumber', row);
+  for (let column = 1; column <= 6; column++) {
+    let $column = $('<div>');
+    $row.append($column);
+    $column.addClass('empty-column');
+    $column.attr('columnNumber', column);
+    }
+  }
+}
 
 $(() => {
-
+makeBoard();
 })
