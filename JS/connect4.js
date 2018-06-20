a358cf10f96f00fc547b6cec254679bd4a45ef78
Brantley Burton <brantleyburton@Brantleys-MacBook-Air.local>
2018-06-19 21:37:00 -0400

    able to check for a connect 2

--- JS/connect4.js
+++ JS/connect4.js
@@ -5,45 +5,53 @@ const $changeBackground = (event) => {
   console.log('executing background changeBackground')
 //Alternates between the two colors
   if (alternate === true) {
-    $clickedCircle.removeClass('empty-column')
+    $clickedCircle.removeClass('empty')
     $clickedCircle.addClass('orange');
     alternate = false;
   } else {
-    $clickedCircle.removeClass('empty-column')
+    $clickedCircle.removeClass('empty')
     $clickedCircle.addClass('green');
     alternate = true
   }
-  checkWinner($clickedCircle);
+  checkWinner($clickedCircle, 0, 0);
 }
 //make a board
 const $circle = $('.circle');
 const makeBoard = () => {
-  for (let row = 1; row <= 7; row++) {
-    let $row = $('<div>');
-    $row.addClass('row');
-    $row.attr('rowNumber', row);
-    for (let column = 1; column <= 6; column++) {
-      let $column = $('<div>');
-      $row.append($column);
-      $column.addClass('empty-column');
-      $column.attr('rowNumber', row)
-      $column.attr('columnNumber', column);
-      $column.on('click', $changeBackground)
-      $row.append($column);
+  for (let column = 1; column <= 6; column++) {
+    let $column = $('<div>');
+    $column.addClass('column');
+    $column.attr('columnNumber', column);
+    for (let row = 1; row <= 7; row++) {
+      let $row = $('<div>');
+      // $row.append($column);
+      $row.addClass('empty');
+      $row.attr('columnNumber', row);
+      $row.attr('rowNumber', column)
+      $row.on('click', $changeBackground)
+      $column.append($row);
     }
-    $('.board').append($row);
+    $('.board').append($column);
   }
 }
 // let currentRow = 0;
-const checkWinner = ($clickedCircle) => {
+const checkWinner = ($clickedCircle, row, column) => {
   console.log('starting checkWinner');
   console.log($clickedCircle.attr('rowNumber'));
-  let currentRow = $clickedCircle.attr('rowNumber');
-  let currentCol = $clickedCircle.attr('columnNumber');
-  // currentCol++
-  $(`div[rownumber=${currentRow}][columnnumber=${currentCol}]`)
-  console.log(`My row number is  ${currentRow}`)
-  console.log(`My column number is  ${currentCol}`)
+  let currentRow = Number($clickedCircle.attr('rowNumber'));
+  let currentCol = Number($clickedCircle.attr('columnNumber'));
+  let currentClass = $clickedCircle.attr('class');
+  let nextCircleRow = currentRow + row;
+  let nextCircleColumn = currentCol + column;
+  let $nextCircle = $(`div[rownumber=${nextCircleRow}][columnnumber=${nextCircleColumn}]`)
+  if ($nextCircle.attr('class') == currentClass){
+    console.log('same class');
+  } else {
+    console.log('no match');
+  }
+  console.log(`My row number is ${nextCircleRow}`)
+  console.log(`My column number is ${nextCircleColumn}`)
+  console.log(`My circle class is ${currentClass}`)
 }
 
 
